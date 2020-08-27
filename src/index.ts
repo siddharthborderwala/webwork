83a3ecfbc851a658d6fb544c99ddeba0f271f668
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-08-28 01:22:55 +0530

    Separate Model class for common functionality

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,3 @@ import axios from 'axios';
 import { User } from './models/User';
 
 axios.defaults.baseURL = `${process.env.API_URL}`;
-
-const user = new User({ name: 'Ada Lovelace', age: 19 });
-
-user.on('save', () => {
-	console.log(user);
-});
-
-user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,9 @@
-import { Eventing } from './Eventing';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
-import { Sync } from './Sync';
-import { AxiosResponse } from 'axios';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 
-const resource: string = 'users';
+const resourceUrl: string = 'users';
 
 export interface UserProps {
 	id?: number;
@@ -11,51 +11,12 @@ export interface UserProps {
 	age?: number;
 }
 
-export class User {
-	private events: Eventing = new Eventing();
-	private sync: Sync<UserProps> = new Sync<UserProps>(resource);
-	private attributes: Attributes<UserProps>;
-
-	constructor(attrs: UserProps) {
-		this.attributes = new Attributes<UserProps>(attrs);
-	}
-
-	get on() {
-		return this.events.on;
-	}
-
-	get trigger() {
-		return this.events.trigger;
-	}
-
-	get get() {
-		return this.attributes.get;
-	}
-
-	set(update: UserProps): void {
-		this.attributes.set(update);
-		this.events.trigger('change');
-	}
-
-	fetch(): void {
-		const id = this.attributes.get('id');
-
-		if (typeof id !== 'number') throw new Error('Cannot fetch without an id');
-
-		this.sync.fetch(id).then((response: AxiosResponse): void => {
-			this.set(response.data);
-		});
-	}
-
-	save(): void {
-		this.sync
-			.save(this.attributes.getAll())
-			.then((response: AxiosResponse): void => {
-				this.set({ id: response.data['id'] });
-				this.trigger('save');
-			})
-			.catch(error => {
-				this.trigger('error');
-			});
+export class User extends Model<UserProps> {
+	static buildUser(attrs: UserProps): User {
+		return new User(
+			new Attributes<UserProps>(attrs),
+			new Eventing(),
+			new ApiSync<UserProps>(resourceUrl)
+		);
 	}
 }
