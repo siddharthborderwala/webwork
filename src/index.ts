76ebc889da5f9fe4589099c2998e62c6bbcfe890
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-09-14 18:44:29 +0530

    Create seperate views

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 const root = document.getElementById('root');
 
 if (!!root) {
-	const userForm = new UserForm(root, user);
+	const userForm = new UserEdit(root, user);
 	userForm.render();
+	console.log(userForm);
 } else {
 	throw new Error('Root element not found');
 }
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,22 +1,18 @@
-import { User } from '../models/User';
+import { User, UserProps } from '../models/User';
+import { View } from './View';
 
-export class UserForm {
-	constructor(public parent: Element, public model: User) {
-		this.bindModel();
-	}
-
-	bindModel(): void {
-		this.model.on('change', (): void => {
-			this.render();
-		});
-	}
-
+export class UserForm extends View<User, UserProps> {
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:button.set-age': this.onSetAgeClick,
 			'click:button.set-name': this.onSetNameClick,
+			'click:button.save-model': this.onSaveClick,
 		};
 	}
+
+	onSaveClick = (): void => {
+		this.model.save();
+	};
 
 	onSetNameClick = (): void => {
 		const input = this.parent.querySelector('input');
@@ -34,36 +30,11 @@ export class UserForm {
 	template(): string {
 		return `
       <div>
-        <h1>User Form</h1>
-				<div>User name: ${this.model.get('name')}</div>
-				<div>User age: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}" />
 				<button class="set-name">Change Name!</button>
 				<button class="set-age">Set random age</button>
+				<button class="save-model">Save</button>
       </div>
     `;
-	}
-
-	bindEvents(fragment: DocumentFragment): void {
-		const eventsMap = this.eventsMap();
-
-		for (let eventKey in eventsMap) {
-			const [eventName, selector] = eventKey.split(':');
-
-			fragment.querySelectorAll(selector).forEach(element => {
-				element.addEventListener(eventName, eventsMap[eventKey]);
-			});
-		}
-	}
-
-	render(): void {
-		this.parent.innerHTML = '';
-
-		const templateElement = document.createElement('template');
-		templateElement.innerHTML = this.template();
-
-		this.bindEvents(templateElement.content);
-
-		this.parent.append(templateElement.content);
 	}
 }
